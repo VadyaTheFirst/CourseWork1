e26a5805e8f33134e4d1f8567305ee7f3d9e93a1
vadym <vadikhide@yandex.ru>
2022-10-29 02:57:10 +0300

    Курсовая работа 1 Базовый уровень Добавил остальные 7 элементов

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,21 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Employee[] Employees = new Employee[3];
+        Employee[] Employees = new Employee[10];
         Employees[0]=new Employee("Василий","Андреевич","ХХХ",1200,5);
         Employees[1]=new Employee("Александр","Андреевич","УУУ",1000,1);
         Employees[2]=new Employee("Сергей","Федорович","ЗЗЗ",12000,4);
+        Employees[3]=new Employee("Василий","Андреевич","ХХХ",200,5);
+        Employees[4]=new Employee("Александр","Андреевич","УУУ",1000,1);
+        Employees[5]=new Employee("Алеся","Николаевна","ЗаЗ",15000,4);
+        Employees[6]=new Employee("Василий","Андреевич","ХХу",1200,5);
+        Employees[7]=new Employee("Александр","Андреевич","УУУ",1000,1);
+        Employees[8]=new Employee("Колян","Федорович","ЗЗЗ",12000,4);
+        Employees[9]=new Employee("Василий","Андреевич","ХХХ",1200,5);
+
+
+
+
         EmployeesAction.EmployeesCout(Employees);
         EmployeesAction.SumSalary(Employees);
         EmployeesAction.MaxSalary(Employees);
